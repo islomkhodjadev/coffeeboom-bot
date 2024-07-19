5134ea50a0e8a1303d901303212e9819a5399490
Islomkhodja <136736159+islomkhodjadev@users.noreply.github.com>
2024-07-19 08:41:42 +0500

    Update tests.py

--- botAdmin/tests.py
+++ botAdmin/tests.py
@@ -3,7 +3,7 @@ from aiogram.types.input_file import BufferedInputFile
 from aiogram.enums.parse_mode import ParseMode
     
 from aiogram import Bot
-bot = Bot(token="6918871508:AAEybCmRHm_3LVEs7Q9Vwj4icwqjWU7VfzQ")
+bot = Bot(token="")
 
     
 from asgiref.sync import sync_to_async
